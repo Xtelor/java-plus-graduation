b1dbdf8408cce80b9bfc818986401404d661588e
unknown <steklovnikolya@yandex.ru>
2026-01-11 22:14:00 +0300

    corrections

--- stats-service/stats-server/src/main/java/ru/practicum/EndpointHitController.java
+++ stats-service/stats-server/src/main/java/ru/practicum/EndpointHitController.java
@@ -3,6 +3,8 @@ package ru.practicum;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
 import java.util.List;
 
 @RestController
